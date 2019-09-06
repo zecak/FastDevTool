17b21b2dea993c503c29d5a6a511915cf67a9243
zecak <zecak@AA-PC>
2019-09-06 17:07:01 +0800

    添加fontawesome字体

---
+++ RunTaskForAny/MEFPlugins/WPF.MEFPlugin.Template1/UserControl1View.xaml.cs
@@ -0,0 +1,33 @@
+﻿using RunTaskForAny.Common.MEF;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF.MEFPlugin.Template1
+{
+
+    /// <summary>
+    /// UserControl1.xaml 的交互逻辑
+    /// </summary>
+    public partial class UserControl1View : UserControl
+    {
+        public UserControl1View()
+        {
+            InitializeComponent();
+        }
+
+
+    }
+}
---
+++ RunTaskForAny/MEFPlugins/WPF.MEFPlugin.Template1/UserControl1ViewModel.cs
@@ -0,0 +1,31 @@
+﻿using RunTaskForAny.Common.MEF;
+using Stylet;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF.MEFPlugin.Template1
+{
+    [Export(typeof(IPluginForViewModel))]
+    [CustomExportMetadata("Plugin2", "这是第2个插件", "snake", 1, "1.0")]
+    public class UserControl1ViewModel : Screen, IPluginForViewModel
+    {
+        public Person PersonInfo { get; set; } = new Person() { ID = 0, FamilyName = "dd", GivenNames = "hh" };
+        public string TitleInfo { get; set; } = "Def";
+
+        public UserControl1ViewModel()
+        {
+            PersonInfo = new Person() { ID = 0, FamilyName = "插件2", GivenNames = "1" };
+            TitleInfo = "插件2 版本1";
+        }
+
+        public void ShowBox()
+        {
+            System.Windows.MessageBox.Show(TitleInfo);
+        }
+
+    }
+}
